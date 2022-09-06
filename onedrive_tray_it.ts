8ee7e0bfc70d5ebc9b93ce43054f3b35bde508b5
renbag <renbag@gmail.com>
2022-09-06 10:33:26 +0200

    Add files via upload

---
+++ onedrive_tray_it.ts
@@ -0,0 +1,263 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>IconInfo</name>
+    <message>
+        <location filename="iconinfo.cpp" line="17"/>
+        <source>Blue</source>
+        <translation type="unfinished">Blu</translation>
+    </message>
+    <message>
+        <location filename="iconinfo.cpp" line="17"/>
+        <source>White</source>
+        <translation type="unfinished">Bianco</translation>
+    </message>
+    <message>
+        <location filename="iconinfo.cpp" line="17"/>
+        <source>Red</source>
+        <translation type="unfinished">Rosso</translation>
+    </message>
+    <message>
+        <location filename="iconinfo.cpp" line="17"/>
+        <source>Green</source>
+        <translation type="unfinished">Verde</translation>
+    </message>
+    <message>
+        <location filename="iconinfo.cpp" line="17"/>
+        <source>Black</source>
+        <translation type="unfinished">Nero</translation>
+    </message>
+</context>
+<context>
+    <name>MoreColorsDialog</name>
+    <message>
+        <location filename="morecolors.cpp" line="14"/>
+        <source>More colors</source>
+        <translation type="unfinished">Più colori</translation>
+    </message>
+    <message>
+        <location filename="morecolors.cpp" line="18"/>
+        <source>Icon color</source>
+        <translation type="unfinished">Colore dell&apos;icona</translation>
+    </message>
+    <message>
+        <location filename="morecolors.cpp" line="37"/>
+        <source>Color your clouds...</source>
+        <translation type="unfinished">Colora le tue nuvole...</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="54"/>
+        <source>OneDrive</source>
+        <translation type="unfinished">OneDrive</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="54"/>
+        <source>I couldn&apos;t detect any system tray on this system.</source>
+        <translation type="unfinished">Non ho potuto trovare nessun system tray.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="window.cpp" line="51"/>
+        <location filename="window.cpp" line="437"/>
+        <source>Recent events</source>
+        <translation type="unfinished">Eventi recenti</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="57"/>
+        <source>OneDrive started</source>
+        <translation type="unfinished">OneDrive avviato</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="132"/>
+        <location filename="window.cpp" line="133"/>
+        <source>Synchronization suspended</source>
+        <translation type="unfinished">Sincronizzazione sospesa</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="143"/>
+        <source>Synchronization restarted</source>
+        <translation type="unfinished">Sincronizzazione riavviata</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="186"/>
+        <source>Synchronizing...</source>
+        <translation type="unfinished">Sincronizzazione in corso...</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="219"/>
+        <location filename="window.cpp" line="452"/>
+        <source>Free space: </source>
+        <translation type="unfinished">Spazio libero: </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="230"/>
+        <source>Creating local directory</source>
+        <translation type="unfinished">Crea cartella locale</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="238"/>
+        <source>Create directory</source>
+        <translation type="unfinished">Crea cartella</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="247"/>
+        <source>Rename</source>
+        <translation type="unfinished">Rinomina</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="259"/>
+        <source>Deleting</source>
+        <translation type="unfinished">Elimina</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="268"/>
+        <source>Uploading</source>
+        <translation type="unfinished">Carica verso OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="280"/>
+        <source>Downloading</source>
+        <translation type="unfinished">Scarica da OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="297"/>
+        <source>Sync complete</source>
+        <translation type="unfinished">Sincronizzazione completata</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="344"/>
+        <source>About</source>
+        <translation type="unfinished">A proposito di OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="346"/>
+        <source>Run and control OneDrive from the system tray.&lt;br&gt;This is a simple program to create a system tray icon and display program status for onedrive client developed by abraunegg.&lt;br&gt;&lt;br&gt;To see the sincronization progress, change icon color, open onedriver, etc. you can click in the icon on system tray.&lt;br&gt;&lt;br&gt;The program was written in C++ using lib Qt 5.13.0.&lt;br&gt;&lt;br&gt;To use the program you must first compile and install the onedrive client available at https://github.com/abraunegg/onedrive.&lt;br&gt;&lt;br&gt;So many thanks to&lt;ul&gt;&lt;li&gt;abraunegg (https://github.com/abraunegg/onedrive)&lt;/li&gt;&lt;li&gt;Daniel Borges Oliveira who developed the first version of this program (https://github.com/DanielBorgesOliveira/onedrive_tray)&lt;/li&gt;&lt;/ul&gt;&lt;br&gt;Feel free to clone and improve this program (https://github.com/bforest76/onedrive_tray).&lt;br&gt;</source>
+        <translation type="unfinished">Avvia e controlla OneDrive dal system tray.&lt;br&gt;Questo è un semplice programma per creare un&apos;icona nel system tray e mostrare lo stato del programma OneDrive client sviluppato da abraunegg.&lt;br&gt;&lt;br&gt;Per visualizzare il progresso della sincronizzazione, cambiare il colore dell&apos;icona, aprire OneDrive, ecc. fare click sull&apos;icona nel system tray.&lt;br&gt;&lt;br&gt;Il programma è stato scritto in C++ usando lib Qt 5.13.0.&lt;br&gt;&lt;br&gt;Per usare il programma occorre prima installare il client OneDrive disponibile all&apos;indirizzo https://github.com/abraunegg/onedrive.&lt;br&gt;&lt;br&gt;Particolari ringraziamenti a&lt;ul&gt;&lt;li&gt;abraunegg (https://github.com/abraunegg/onedrive)&lt;/li&gt;&lt;li&gt;Daniel Borges Oliveira, che ha sviluppato la prima versione di questo programma (https://github.com/DanielBorgesOliveira/onedrive_tray)&lt;/li&gt;&lt;/ul&gt;&lt;br&gt;Questo programma può essere copiato e modificato liberamente (https://github.com/bforest76/onedrive_tray).&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="387"/>
+        <source>OneDrive is running with the PID </source>
+        <translation type="unfinished">OneDrive è in esecuzione con il PID </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="393"/>
+        <source>OneDrive is not running by some reason. Please restart the program.</source>
+        <translation type="unfinished">OneDrive non è in esecuzione per qualche motivo. Per favore riavviare il programma.</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="403"/>
+        <source>OneDrive</source>
+        <translation type="unfinished">OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="445"/>
+        <source>Show icon</source>
+        <translation type="unfinished">Mostra l&apos;icona</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="455"/>
+        <source>Not started</source>
+        <translation type="unfinished">Non avviato</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="458"/>
+        <source>&amp;Recent events</source>
+        <translation type="unfinished">&amp;Eventi recenti</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="461"/>
+        <source>&amp;Open OneDrive folder</source>
+        <translation type="unfinished">&amp;Apri la cartella di OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="467"/>
+        <source>&amp;Restart the synchronization</source>
+        <translation type="unfinished">&amp;Riavvia la sincronizzazione</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="471"/>
+        <source>&amp;Suspend the synchronization</source>
+        <translation type="unfinished">&amp;Sospendi la sincronizzazione</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="493"/>
+        <source>&amp;More colors...</source>
+        <translation type="unfinished">&amp;Più colori...</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="499"/>
+        <source>&amp;Quit OneDrive</source>
+        <translation type="unfinished">&amp;Termina OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="502"/>
+        <source>&amp;About OneDrive</source>
+        <translation type="unfinished">A &amp;proposito di OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="531"/>
+        <source>Icon color</source>
+        <translation type="unfinished">Colore dell&apos;icona</translation>
+    </message>
+</context>
+<context>
+    <name>Window_1</name>
+    <message>
+        <location filename="window_1.cpp" line="27"/>
+        <location filename="window_1.cpp" line="52"/>
+        <source>Configuration...</source>
+        <translation type="unfinished">Configurazione...</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="40"/>
+        <source>OneDrive</source>
+        <translation type="unfinished">OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="41"/>
+        <source>The program will keep running in the system tray. To terminate the program, choose &lt;b&gt;Quit&lt;/b&gt; in the context menu of the system tray entry.</source>
+        <translation type="unfinished">Il programma continuerà l&apos;esecuzione nel system tray. Per terminare il programma selezionare &lt;b&gt;Termina&lt;/b&gt; nel menu contestuale del system tray.</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="84"/>
+        <source>Monitor Interval</source>
+        <translation type="unfinished">Intervallo di monitoraggio</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="91"/>
+        <source>Files/Directories to skip:</source>
+        <translation type="unfinished">File/Cartelle da ignorare:</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="92"/>
+        <source>~*|.~*|*.db|Thumbs.db|*encryptable*|~$*|*.pyc|log.*|*.swp|*.swo</source>
+        <translation type="unfinished">~*|.~*|*.db|Thumbs.db|*encryptable*|~$*|*.pyc|log.*|*.swp|*.swo</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="94"/>
+        <source>Syncronization Folder:</source>
+        <translation type="unfinished">Cartella di sincronizzazione:</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="95"/>
+        <source>~/OneDrive</source>
+        <translation type="unfinished">~/OneDrive</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="97"/>
+        <source>Additional Configuration:</source>
+        <translation type="unfinished">Configurazioni addizionali:</translation>
+    </message>
+    <message>
+        <location filename="window_1.cpp" line="101"/>
+        <source>Save</source>
+        <translation type="unfinished">Salva</translation>
+    </message>
+</context>
+</TS>
